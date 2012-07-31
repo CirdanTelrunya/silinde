071884e4a533f9bb278c96cbf638a3bcea087483
bug <bug@Lorlunir.(none)>
2012-07-31 19:32:49 +0200

    Ajout du menu contextuel

--- Main.py
+++ Main.py
@@ -13,14 +13,26 @@ class TreeManipulator(QGroupBox):
         self.ui.setupUi(self)
         self.model = TreeItemModel()
         self.ui.treeView.setModel(self.model)
+        self.ui.treeView.setContextMenuPolicy(Qt.CustomContextMenu)
+        
+
         #index = self.model.index(0,0, QtCore.QModelIndex( ))
         #self.model.insertNode(index)
         QObject.connect(self.ui.pushButton, SIGNAL("clicked()"), self.ajout)
-
+        QObject.connect(self.ui.treeView,SIGNAL('customContextMenuRequested(QPoint)'), self.ctxMenu)
+        self.actionAjout = QAction("Ajout", self);
+        QObject.connect(self.actionAjout, SIGNAL("triggered()"), self.ajout)
+        
     def ajout(self):
         indices = self.ui.treeView.selectedIndexes()
         if len(indices) == 1:
             self.model.insertNode(indices[0])
+            
+    def ctxMenu(self, point):
+        print "OK"
+        menu = QMenu(self)        
+        menu.addAction(self.actionAjout);
+        menu.popup(self.ui.treeView.mapToGlobal(point))
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
