7bb2cf88a3365e8ae6ebcfd6ec4b3c0867eb138a
CirdanTelrunya <ronanbillon@hotmail.com>
2012-08-20 17:51:04 +0200

    Beautify the code

--- TreeNode.py
+++ TreeNode.py
@@ -16,12 +16,14 @@ class TreeNode(object):
         # Structure
         self._parent = parent
         self._children = []
+    # end def __init__
 
     def __str__(self):
         assert isinstance(self._name, (str, unicode))
         return _name
+    # end def __str__
 
-    def insertChild(self, node, row = None):
+    def insertChild(self, node, row=None):
         assert isinstance(node, TreeNode)
         node._parent = self
         if row is None:
@@ -29,42 +31,55 @@ class TreeNode(object):
         else:
             assert isinstance(row, int)
             self._children.insert(row, node)
+	# end if
+    # end def insertChild
 
     def name(self):
         assert isinstance(self._name, (str, unicode))
         return self._name
+    # end def name
 
     def setName(self, name='Untitled'):
         assert isinstance(name, (str, unicode))
         self._name = name
+    # end def setName
 
     def type(self):
         assert isinstance(self._type, (str, unicode))
         return self._type
+    # end def type
 
     def id(self):
         return self._id
+    # end def id
 
     def parent(self):
         return self._parent
+    # end def parent
 
     def setParent(self, parent):
         assert isinstance(parent, TreeNode)
         self._parent = parent
+    # end def setParent
 
     def child(self, row):
         return self._children[row]
+    # end def child
 
     def children(self):
         return self._children
-
+    # end def children
 
     def childCount(self):
         return len(self._children)
+    # end def childCount
 
     def row(self):
         if self._parent:
             return self._parent._children.index(self)
+	# end if
         return 0
+    # end def row
+# end class TreeNode
 
 
--- TreeNodeItemModel.py
+++ TreeNodeItemModel.py
@@ -16,6 +16,8 @@ class TreeNodeItemModel(QtCore.QAbstractItemModel):
                     parent=rootNode))
         else:
             self._rootNode.insertChild(rootNode)
+	# end if
+    # end def __init__
 
     def index(
         self,
@@ -23,61 +25,78 @@ class TreeNodeItemModel(QtCore.QAbstractItemModel):
         column,
         parent,
         ):
+
         if not self.hasIndex(row, column, parent):
             return QtCore.QModelIndex()
+	# end if
         if row < 0 or column < 0 or row >= self.rowCount(parent) \
             or column >= self.columnCount(parent):
             return QtCore.QModelIndex()
+	# end if
         if not parent.isValid():
             parentNode = self._rootNode
         else:
             parentNode = parent.internalPointer()
-
+	# end if
         childNode = parentNode.child(row)
         if childNode:
             return self.createIndex(row, column, childNode)
         else:
             return QtCore.QModelIndex()
+	# end if
+    # end def index
 
     def parent(self, index):
         if not index.isValid():
             return QtCore.QModelIndex()
+	# end if
         childItem = index.internalPointer()
         parentNode = childItem.parent()
         if parentNode == self._rootNode:
             return QtCore.QModelIndex()
+	# end if
         return self.createIndex(parentNode.row(), 0, parentNode)
+    # end def parent
 
     def rowCount(self, parent):
         if parent.column() > 0:
             return 0
+	# end if
         if not parent.isValid():
             parentNode = self._rootNode
         else:
             parentNode = parent.internalPointer()
-
+	# end if
         return parentNode.childCount()
+    # end def rowCount
 
     def columnCount(self, index):
         return 1
+    # end def columnCount
 
     def data(self, index, role):
         if not index.isValid():
             return QtCore.QVariant()
+	# end if
         node = index.internalPointer()
         if role == QtCore.Qt.DisplayRole:
             return str(node.name())
+	# end if
+    # end def data
 
     def insertNode(self, parent, node):
         assert isinstance(node, TreeNode)
         parentNode = parent.internalPointer()
         if parentNode is None:
             parentNode = self._rootNode
+	# end if
         newRow = parentNode.childCount()
         # Insert the Node
         self.beginInsertRows(parent, newRow, newRow)
         parentNode.insertChild(node, newRow)
         self.endInsertRows()
+    # end def insertNode
+# end class TreeNodeItemModel
 
 
 if __name__ == '__main__':
@@ -93,3 +112,4 @@ if __name__ == '__main__':
     model.insertNode(index, TreeNode('Plop'))
     dialog.exec_()
     app.closeAllWindows()
+# end if
