734b6e8f9c42ab1cd7b215b407d2c3704d847b93
bug <bug@Lorlunir.(none)>
2012-09-03 20:16:41 +0200

    Add Thread in SoundMgr

--- sport/SoundMgr.py
+++ sport/SoundMgr.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*- 
 
 from pygame import mixer
+import threading
 
 class SoundMgr(object):
     class __SoundMgr():
         def __init__(self):
             self.sounds = {}
             mixer.init()
+        def __del__(self):
+            mixer.quit()
+            print "delete SoundMgr"
         def __str__(self):
             return str(self.sounds)
         def add(self, soundName, soundFile):
@@ -18,7 +22,7 @@ class SoundMgr(object):
     
     instance = None
     
-    def __new__(c):
+    def __new__(self):
         if not SoundMgr.instance:
             SoundMgr.instance = SoundMgr.__SoundMgr()
         return SoundMgr.instance
@@ -29,9 +33,54 @@ class SoundMgr(object):
     def __setattr__(self, attr, val):
         return setattr(self.instance, attr, val)
 
+class SoundWorker(threading.Thread):
+    def __init__(self, condition, group=None, target=None, name=None, args=(), kwargs={}):
+        super(SoundWorker, self).__init__(group, target, name, args, kwargs)
+        self._continue = True
+        self._condition = condition
+        self._sound = None
+
+    def stop(self):
+        self._continue = False
+
+    def play(self, sound):
+        self._sound = sound
+
+    def run(self):
+        self._condition.acquire()
+        while True:
+            self._condition.wait()
+            self._sound.play()
+            while mixer.get_busy():
+                pass
+            print "OK !"
+            if not self._continue:
+                break            
+        self._condition.release()
+        pass
+        
+
 if __name__ == "__main__":
+    condition = threading.Condition()
+    worker = SoundWorker(condition)
+    worker.start()
+    
+
     x = SoundMgr()
     x.add('anvil', '/usr/lib/openoffice/basis3.2/share/gallery/sounds/ANVIL.WAV')
     x.play('anvil')
     x.play('anvil')
     
+    s = mixer.Sound('/usr/lib/openoffice/basis3.2/share/gallery/sounds/ANVIL.WAV')
+    
+    condition.acquire()
+    worker.play(s)
+    condition.notify()
+    condition.release()
+
+    condition.acquire()
+    worker.stop()
+    condition.notify()
+    condition.release()
+    
+    SoundMgr.instance = None
